a3672a5050a94e313538c6b8014564f7215ba359
Korben-Dallas-React <borozan.eugen@gmail.com>
2023-02-08 00:06:52 +0200

    vercel

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -588,9 +588,59 @@ fontOptions.forEach((e)=>{
 form.addEventListener("submit", (e)=>{
     e.preventDefault();
     const value = form.search.value;
-    console.log(value);
-    const data = (0, _helper.dictionary)(value).then();
+    (0, _helper.dictionary)(value).then((value)=>renderResult(value));
 });
+const audioTrigger = document.querySelector(".audio");
+const audio = document.querySelector("audio");
+audioTrigger.addEventListener("click", ()=>{
+    audio.play();
+});
+/**/ const word = document.querySelector(".phonetica-text__word");
+const subtitle = document.querySelector(".phonetica-text__subtitle");
+const meaningHeading = document.querySelector(".meaning-header__text");
+const meaning = document.querySelector(".meaning");
+function renderResult(data) {
+    meaning.innerHTML = "";
+    const [object] = data;
+    const [, phonetic] = object.phonetics;
+    word.innerText = object.word;
+    subtitle.innerText = phonetic.text;
+    audio.src = phonetic.audio;
+    console.log(phonetic.audio);
+    object.meanings.forEach((e, index)=>{
+        meaning.insertAdjacentHTML("beforeend", `
+      <section class="meaning">
+          <header class="meaning-header">
+            <p class="meaning-header__text">${e.partOfSpeech}</p>
+            <span class="line"></span>
+          </header>
+          <div class="meaning-content">
+            <p>Meaning</p>
+            <ul class="meaning-list-${index}">
+            </ul>
+          </div>
+          <div class="meaning-synonymes-${index}">
+            <p>Synonymes</p>
+          </div>
+        </section>
+    `);
+        const meaningList = document.querySelector(`.meaning-list-${index}`);
+        const synonymeList = document.querySelector(`.meaning-synonymes-${index}`);
+        e.definitions.forEach((e)=>{
+            meaningList.insertAdjacentHTML("afterbegin", `
+      <li class="meaning-list__ityem">
+        ${e.definition}
+      </li>
+      `);
+        });
+        e.synonyms.forEach((e)=>{
+            console.log(e);
+            synonymeList.insertAdjacentHTML("beforeend", `
+      <p class="synonym">${e}</p>
+      `);
+        });
+    });
+}
 
 },{"./helpers/helper":"9Dk1s"}],"9Dk1s":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
